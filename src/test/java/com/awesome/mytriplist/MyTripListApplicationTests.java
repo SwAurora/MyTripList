346475599774022d2fed203ab4cc1db722e0632f
SwAurora <rlatjddnr74@gmail.com>
2023-01-18 00:16:46 +0900

    2023-01-18 master(ver1) - 프로젝트 설정

---
+++ src/test/java/com/awesome/mytriplist/MyTripListApplicationTests.java
@@ -0,0 +1,15 @@
+package com.awesome.mytriplist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTripListApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
